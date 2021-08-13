2910112de36ed53cc1a3f5b5578724c348be4589
SulaymonZafarov <sulaymonwork@gmail.com>
2021-08-13 15:32:54 +0500

    HW-3.2

--- DZ3.1/Program.cs
+++ DZ3.1/Program.cs
@@ -6,7 +6,28 @@ namespace DZ3._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var discount3 = 0.03;
+            var discount5 = 0.05;
+            var pAmount1 = 500;
+            var pAmount2 = 1000;
+            Console.WriteLine("Ввидите сумму");
+            var summAmount = Convert.ToInt32(Console.ReadLine());
+
+            if (summAmount > pAmount1 && summAmount < pAmount2)
+            {
+                var sum = summAmount * discount3;
+                Console.Write($"Сумма к оплате с учётом 3% скидкой составляет {pAmount1 - sum}");
+            }
+            else if (summAmount > pAmount2)
+            {
+                var sum = summAmount * discount5;
+                Console.Write($"Сумма к оплате с учётом 5% скилкой составляет {pAmount2 - sum}");
+            }
+            else
+            {
+                Console.Write($"Сумма покупки без скидки= {summAmount}");
+            }
+            
         }
     }
 }
