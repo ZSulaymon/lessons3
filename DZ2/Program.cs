5cbb8e1d2394a0518ee122a96c78096858f005c7
SulaymonZafarov <sulaymonwork@gmail.com>
2021-08-13 22:33:07 +0500

    last commit

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -7,7 +7,61 @@ namespace DZ2
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!");
+            bool isInc = false;
+
+
+            int[] mass = new int[4];
+            Console.WriteLine("Введите 1-число");
+            mass[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2-число");
+            mass[1] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 3-число");
+            mass[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 4-число");
+            mass[3] = Convert.ToInt32(Console.ReadLine());
+
+            int minValue = mass[0];
+            for (int i = 0; i < mass.Length - 1; i++) //делаем тоже самое только для возрастающей последовательности
+                if (mass[i] < mass[i + 1]) isInc = true;
+                else
+                {
+                    isInc = false;
+                    break;
+                }
+
+             if (mass[0] == mass[1] && mass[2] == mass[3])
+            {
+                var summ = mass[0] * mass[1] * mass[2] * mass[3];
+                Console.Write($"произведение чисел = { summ }");
+            }
+
+            else if (!isInc)
+
+            {
+                for (int i = 0; i < mass.Length; i++)
+                {
+                    if (mass[i] < minValue)
+                    {
+                        minValue = mass[i];
+                    }
+
+                }
+                Console.WriteLine($"мин знач - {minValue}");//ес        }        }
+                
+            }
+            
+             
+               
+          //  Console.WriteLine($"мин знач - {minValue}");//ес
+
+             else 
+                //(isInc)
+              {
+                Console.WriteLine($"числа расположены по возрастанию");
+                //isInc = false;
+                //break;
+            }
         }
     }
+
 }
---
+++ DZ3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DZ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 90;
+            var b = 45;
+            var c = 10;
+
+            var sr = b;
+            var max = b;
+            var min = b;
+
+            if (a >= b)  //находим макс знач 
+                max = a;
+            if (max < c)
+                max = c;
+
+            if (a < b)  //находим мин знач
+                min = a;
+            if (min > c)
+                min = c;
+
+            if (b > min && b < max) 
+                sr = b;
+            if (a > min && a < max)
+                sr = a;
+            if (c > min && c < max)
+                sr = c;
+
+            a = max;
+            b = sr;
+            c = min;
+            Console.WriteLine($"{a}, {b}, {c}");
+            
+        }
+    }
+}
